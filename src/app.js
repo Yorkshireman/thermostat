f20f738f27dac6088fd694f0dcd58ea8e4d8f0af
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-18 17:10:44 +0100

    add turn up and turn down abilities

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 var thermostat = new Thermostat();
-var thermostatTemperature = document.getElementById('current_temperature')
+var thermostatTemperature = document.getElementById('current_temperature');
 
 thermostatTemperature.innerHTML = thermostat.temperature;
 
-document.getElementById('increase_temperature').onclick=function() 
-{   thermostat.increaseTemperature();
-    thermostatTemperature.innerHTML = thermostat.temperature;
+document.getElementById('increase_temperature').onclick=function() {
+  thermostat.increaseTemperature();
+  thermostatTemperature.innerHTML = thermostat.temperature;
 };
+
+document.getElementById('decrease_temperature').onclick=function() {
+  thermostat.decreaseTemperature();
+  thermostatTemperature.innerHTML = thermostat.temperature;
+};
+
