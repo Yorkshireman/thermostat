e36cfe5c15bf90c16f0bad47dde38a7be3a54176
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-18 13:52:18 +0100

    thermostat's temp_range is set to low, medium, or high, according to its temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,12 +1,36 @@
 describe('Theromstat', function(){
   var thermostat;
+
   beforeEach(function(){
     thermostat = new Thermostat();
   });
 
   describe('Temperature', function() {
     it('has a default temperature of 20', function() {
-      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('Power Saving mode', function() {
+    it('is on by default', function() {
+      expect(thermostat.isPowerSavingOn).toBe(true);
+    });
+  });
+
+  describe('Colour', function() {
+    it('is green when temperature is below 18', function() {
+      thermostat.setTemperature(17);
+      expect(thermostat.temp_range).toEqual("low");
+    });
+
+    it('is yellow when temperature is 18 or greater and below 25', function() {
+      thermostat.setTemperature(19)
+      expect(thermostat.temp_range).toEqual("medium");
+    });
+
+    it('is red when temperature is anything else', function() {
+      thermostat.setTemperature(27)
+      expect(thermostat.temp_range).toEqual("high");
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,4 +1,23 @@
 var Thermostat = function() {
-  const DEFAULT_TEMPERATURE = 20
-  this.temperature = this.DEFAULT_TEMPERATURE;
+  const DEFAULT_TEMPERATURE = 20;
+
+  this.temperature = DEFAULT_TEMPERATURE;
+  this.isPowerSavingOn = true;
+  this.temp_range = null;
+  this.setTempRange(this.temperature);
+};
+
+Thermostat.prototype.setTemperature = function(new_temperature) {
+  this.temperature = new_temperature
+  this.setTempRange(new_temperature)
+};
+
+Thermostat.prototype.setTempRange = function(temperature) {
+    if(temperature < 18) {
+      this.temp_range = "low";
+    } else if(temperature >= 18 && temperature < 25) {
+      this.temp_range = "medium";
+    } else {
+      this.temp_range = "high";
+    }
 };
